44da472c60ee9413838083427f2b2e2c5540da47
christa-baccas <christa.baccas@gmail.com>
2021-09-25 12:41:38 -0500

    added html content and css styling

---
+++ script.js
@@ -0,0 +1,16 @@
+
+// WHEN I click the start button a timer starts and I am presented with a question
+    // addEventListener function for timer
+    // text should disappear and questions should appear
+
+// WHEN I answer a question I am presented with another question
+    //
+
+// WHEN I answer a question incorrectly time is subtracted from the clock
+    // if statment subtract time when incorrect answer is clicked
+
+// WHEN all questions are answered or the timer reaches 0 then the game is over
+    //
+
+// WHEN the game is over I can save my initials and my score
+    // location storage (string)
