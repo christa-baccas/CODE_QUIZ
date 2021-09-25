c98133429e991fbe8bb6a33959935236bacd8698
christa-baccas <christa.baccas@gmail.com>
2021-09-25 17:43:46 -0500

    added function to the start button and time starts

--- script.js
+++ script.js
@@ -1,4 +1,58 @@
 
+var startBtn = document.getElementById('startBtn');
+var homePage = document.getElementById('container');
+var timer = document.getElementById('time');
+var questions = [
+    {
+        question: 'what color is the sky?',
+        choices:['green', 'blue', 'yellow', 'red'],
+        answer: 'blue'
+    },
+    {
+        question: 'what color is the ocean?',
+        choices:['green', 'blue', 'yellow', 'red'],
+        answer: 'blue'
+    },
+    {
+        question: 'what color is the grass?',
+        choices:['green', 'blue', 'yellow', 'red'],
+        answer: 'green'
+    },
+    {
+        question: 'what color is the fire truck?',
+        choices:['green', 'blue', 'yellow', 'red'],
+        answer: 'red'
+    },
+    {
+        question: 'what color is the taxi?',
+        choices:['green', 'blue', 'yellow', 'red'],
+        answer: 'yellow'
+    }
+]
+
+startBtn.addEventListener("click", function() {
+    homePage.setAttribute('style', 'display: none');
+
+    var timeleft = 60;
+    var countdown = setInterval (function(){
+    document.getElementById("time").textContent = 'Time: ' + timeleft + ' seconds';
+    timeleft -= 1;
+    if(timeleft <= 0){
+        clearInterval(downloadTimer);
+    }
+    }, 1000);
+
+});
+
+function showQuestions(){
+    var quest = document.createElement('div');
+    quest.append(questions.question);
+}
+// showQuestions();
+
+
+
+
 // WHEN I click the start button a timer starts and I am presented with a question
     // addEventListener function for timer
     // text should disappear and questions should appear
